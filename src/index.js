c5fb9e344afcd19142bad9de6631d49a9c33bb33
Ashlyn Mitros <ashlyndmitros@gmail.com>
2017-10-23 13:39:39 -0600

    initial set up and overview

--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-
-import App from './components/app';
-import reducers from './reducers';
-
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// Creat a new component that will produce some HTML
+const App = () => {
+  return <div>Hello World</div>
+}
+// Take this component's generated HTML and put it
+// on the page ( render it to the DOM )
+ReactDOM.render(<App />, document.querySelector('.container'));
